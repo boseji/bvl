5e9a54d0e05705d9137986686a0c83de0ee218c1
boseji <boseji@users.noreply.github.com>
2025-06-19 16:33:09 +0530

    पुनरीक्षण

---
+++ inventory/db.go
@@ -0,0 +1,64 @@
+// db.go - Part of the `inventory` Package
+//
+//     ॐ भूर्भुवः स्वः
+//     तत्स॑वि॒तुर्वरे॑ण्यं॒
+//    भर्गो॑ दे॒वस्य॑ धीमहि।
+//   धियो॒ यो नः॑ प्रचो॒दया॑त्॥
+//
+//
+//  बी.वी.एल - बोसजी के द्वारा रचित भंडार लेखांकन हेतु तन्त्राक्ष्।
+// =============================================
+//
+// एक सुगम एवं उपयोगी भंडार संचालन हेतु तन्त्राक्ष्।
+//
+// एक रचनात्मक भारतीय उत्पाद ।
+//
+// bvl - Boseji's Inventory Management Program
+//
+// Easy to use and useful stock, goods and materials handling software.
+//
+// Sources
+// -------
+// https://github.com/boseji/bvl
+//
+// License
+// -------
+//
+//   bvl - Boseji's Inventory Management Program.
+//   Copyright (C) 2025 by Abhijit Bose (aka. Boseji)
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 only
+//   as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+//  SPDX-License-Identifier: GPL-2.0-only
+//  Full Name: GNU General Public License v2.0 only
+//  Please visit <https://spdx.org/licenses/GPL-2.0-only.html> for details.
+//
+
+package inventory
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func OpenDB(dbFile string) *sql.DB {
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	// TODO: Add Pre-processing Steps
+
+	return db
+}
